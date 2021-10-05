43e267b89c11f2506bf7a8adf4dacc2cc874f8b6
Daniela <danisalazarcueto7@gmail.com>
2021-10-05 22:42:07 +0100

    add filter bonus and fix issues

--- src/components/App.js
+++ src/components/App.js
@@ -8,33 +8,51 @@ function App() {
   const [textInput, setTextInput] = useState("");
   const [weekInput, setWeekInput] = useState(false);
   const [weekendInput, setWeekendInput] = useState(false);
+  const [filter, setFilter] = useState("All");
 
-  //Pintar el listado de clubs inicial.
-  const htmlList = clubs.map((oneData, i) => (
-    <li key={i} className="book__item item">
-      <h3 className="item__title">
-        #{i}: {oneData.name}
-      </h3>
-      <p className="item__text">
-        Abierto entre semana:{oneData.openOnWeekdays ? "Sí" : "No"}
-      </p>
-      <p className="item__text">
-        Abierto el fin de semana:{oneData.openOnWeekend ? "Sí" : "No"}
-      </p>
-    </li>
-  ));
+  //Filtrar el listado y luego con map PINTAR el listado de clubs
+
+  const htmlList = () => {
+    return clubs
+      .filter((oneData) => {
+        if (filter === "openOnWeekDays") {
+          return oneData.openOnWeekdays === true;
+        } else if (filter === "openOnWeekend") {
+          return oneData.openOnWeekend === true;
+        }
+        return true;
+      })
+      .map((oneData, i) => {
+        return (
+          <li key={i} className="book__item item">
+            <h3 className="item__title">
+              #{i}: {oneData.name}
+            </h3>
+            <p className="item__text">
+              Abierto entre semana:{oneData.openOnWeekdays ? "Sí" : "No"}
+            </p>
+            <p className="item__text">
+              Abierto el fin de semana:{oneData.openOnWeekend ? "Sí" : "No"}
+            </p>
+          </li>
+        );
+      });
+  };
 
   const handleTextChange = (ev) => {
+    ev.preventDefault();
     const inputValue = ev.currentTarget.value;
     setTextInput(inputValue);
   };
   const handleWeek = (ev) => {
-    const inputValue = ev.currentTarget.checked;
-    setWeekInput(inputValue);
+    setWeekInput(ev.currentTarget.checked);
   };
   const handleWeekend = (ev) => {
-    const inputValue = ev.currentTarget.checked;
-    setWeekendInput(inputValue);
+    setWeekendInput(ev.currentTarget.checked);
+  };
+
+  const handleFilter = (ev) => {
+    setFilter(ev.target.value);
   };
 
   const handleNewClub = (ev) => {
@@ -44,9 +62,14 @@ function App() {
       openOnWeekdays: weekInput,
       openOnWeekend: weekendInput,
     };
+    //con la función de estado, cambio los valores
+    setTextInput("");
+    setWeekInput(false);
+    setWeekendInput(false);
 
     //añado el objeto a la lista de clubs
     clubs.push(newClub);
+    //console.log(newClub);
 
     // digo a react que la lista de clubs cambio
     setClubs([...clubs]);
@@ -56,15 +79,35 @@ function App() {
     <div className="page">
       <header className="header">
         <h1 className="header__title">Mis clubs</h1>
+        <select value={filter} onChange={handleFilter}>
+          <option value="All">Todos</option>
+          <option value="openOnWeekDays">Abren entre semana</option>
+          <option value="openOnWeekend">Abren los fines de semana</option>
+        </select>
       </header>
-      <ul className="book__list">{htmlList}</ul>
+      <ul className="book__list">{htmlList()}</ul>
       <form className="formulary">
         <label htmlFor="text">Nombe del club</label>
-        <input onChange={handleTextChange} type="text" name="text" />
+        <input
+          onChange={handleTextChange}
+          type="text"
+          name="text"
+          value={textInput}
+        />
         <label htmlFor="checkbox">¿Abre entre semana?</label>
-        <input onChange={handleWeek} type="checkbox" />
+        <input
+          onChange={handleWeek}
+          type="checkbox"
+          id="week"
+          checked={weekInput}
+        />
         <label htmlFor="checkbox">¿Abre los fines de semana?</label>
-        <input onChange={handleWeekend} type="checkbox" />
+        <input
+          onChange={handleWeekend}
+          type="checkbox"
+          id="weekend"
+          checked={weekendInput}
+        />
         <button type="button" onClick={handleNewClub} className="button">
           Añadir un nuevo club
         </button>
